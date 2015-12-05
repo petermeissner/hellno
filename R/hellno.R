4dedb3dffb2b55ab99545e8955ad2b99e2145569
Peter Meissner <retep.meissner@gmail.com>
2015-12-05 16:44:25 +0100

    doc

--- R/hellno.R
+++ R/hellno.R
@@ -5,6 +5,11 @@ HELLNO <- FALSE
 #'
 #' \code{hellno::data.frame()} wraps up \code{base::data.frame()} so that
 #' \code{stringAsFactors} is set to \code{HELLNO} ( \code{== FALSE} ) by default
+#' @param row.names see \code{\link[base]{data.frame}}
+#' @param check.rows see \code{\link[base]{data.frame}}
+#' @param check.names see \code{\link[base]{data.frame}}
+#' @param ... see \code{\link[base]{data.frame}}
+#' @param stringsAsFactors see \code{\link[base]{data.frame}} by default set to \code{FALSE}
 #' @seealso \code{\link[base]{data.frame}}
 data.frame <- function (
     ..., row.names = NULL, check.rows = FALSE,
@@ -21,6 +26,11 @@ data.frame <- function (
 #'
 #' \code{hellno::as.data.frame()} wraps up \code{base::as.data.frame()} so that
 #' \code{stringAsFactors} is set to \code{HELLNO} ( \code{== FALSE} ) by default
+#' @param x see \code{\link[base]{as.data.frame}}
+#' @param row.names see \code{\link[base]{as.data.frame}}
+#' @param optional see \code{\link[base]{as.data.frame}}
+#' @param stringsAsFactors see \code{\link[base]{as.data.frame}} by default set to \code{FALSE}
+#' @param ... see \code{\link[base]{as.data.frame}}
 #' @seealso \code{\link[base]{as.data.frame}}
 as.data.frame <- function (
   x, row.names = NULL, optional = FALSE, stringsAsFactors=HELLNO, ...
