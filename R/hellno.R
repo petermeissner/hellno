52eccaa71929f0cf67a0bd776a30e446364f10f2
Peter Meissner <retep.meissner@gmail.com>
2015-12-14 13:32:59 +0100

    CRAN (getting ready)

--- R/hellno.R
+++ R/hellno.R
@@ -1,25 +1,17 @@
 #' logical constant for FALSE
+#' @export
 HELLNO <- FALSE
 
 #' alternative data.frame() implementation
 #'
 #' \code{hellno::data.frame()} wraps up \code{base::data.frame()} so that
 #' \code{stringAsFactors} is set to \code{HELLNO} ( \code{== FALSE} ) by default
-#' @param row.names see \code{\link[base]{data.frame}}
-#' @param check.rows see \code{\link[base]{data.frame}}
-#' @param check.names see \code{\link[base]{data.frame}}
 #' @param ... see \code{\link[base]{data.frame}}
 #' @param stringsAsFactors see \code{\link[base]{data.frame}} by default set to \code{FALSE}
 #' @seealso \code{\link[base]{data.frame}}
-data.frame <- function (
-    ..., row.names = NULL, check.rows = FALSE,
-    check.names = TRUE, stringsAsFactors = HELLNO
-  )
-{
-  base::data.frame(
-    ..., row.names=row.names, check.rows=check.rows,
-    check.names=check.names, stringsAsFactors=stringsAsFactors
-  )
+#' @export
+data.frame <- function ( ..., stringsAsFactors = HELLNO ) {
+  base::data.frame( ..., stringsAsFactors=stringsAsFactors )
 }
 
 #' alternative as.data.frame() implementation
@@ -32,10 +24,10 @@ data.frame <- function (
 #' @param stringsAsFactors see \code{\link[base]{as.data.frame}} by default set to \code{FALSE}
 #' @param ... see \code{\link[base]{as.data.frame}}
 #' @seealso \code{\link[base]{as.data.frame}}
+#' @export
 as.data.frame <- function (
   x, row.names = NULL, optional = FALSE, stringsAsFactors=HELLNO, ...
-)
-{
+){
   base::as.data.frame(
     x, row.names = NULL, optional = FALSE, stringsAsFactors=stringsAsFactors, ...
   )
